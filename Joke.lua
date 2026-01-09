8ec064be76641fb93ece46b503fda44d0087f39d
keyboardturner <20036792+keyboardturner@users.noreply.github.com>
2026-01-09 13:41:05 -0800

    Midnight API restrictions

--- Joke.lua
+++ Joke.lua
@@ -1,4 +1,12 @@
 SLASH_joke1 = "/joke";
 SlashCmdList["joke"] = function()
-    DoEmote('JOKE')
+    if WOW_PROJECT_ID == WOW_PROJECT_MAINLINE and C_InstanceEncounter and C_InstanceEncounter.IsEncounterInProgress and C_InstanceEncounter.IsEncounterInProgress() then
+        return -- does not work with Midnight API
+    else
+        if C_ChatInfo and C_ChatInfo.PerformEmote then
+            C_ChatInfo.PerformEmote("JOKE");
+        else
+            DoEmote("JOKE");
+        end
+    end
 end
